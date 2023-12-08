a0c6d97182edf31b12d5d744270024d5c7fcae40
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-08 22:07:19 +0100

    Removed services

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import About from './About';
 import Contact from './Contact';
 import HeroSection from './HeroSection';
-import Services from './Services';
+// import Services from './Services';
 import Skills from './Skills';
 import Works from './Works';
 
@@ -10,10 +10,10 @@ const Main = ({nav, handleNav, closeNav}) => {
   return(
     <div onClick={closeNav } className='main'>
         <HeroSection nav={nav} handleNav={handleNav} />
-        <About />
-        <Skills />
-        <Services />
         <Works />
+        <Skills />
+        <About />
+        {/* <Services /> */}
         <Contact />
     </div>
   )
